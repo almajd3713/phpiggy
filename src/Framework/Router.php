aedb26f686d093d624d44f5607e945585d16752b
almajd3713 <almajd3713@gmail.com>
2023-10-08 17:19:30 +0100

    add basic routing

---
+++ src/Framework/Router.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types = 1);
+
+namespace Framework;
+
+class Router {
+  private array $routes = [];
+  
+  public function add(string $method, string $path, $controller) {
+    $this->routes[] = [
+      'path' => $this->normalizePath($path),
+      'method' => $method,
+      'controller' => $controller
+    ];
+  }
+
+  private function normalizePath(string $path) : string {
+    $path = trim($path, '/');
+    $path = "/{$path}/";
+    $path = preg_replace("#[/]{2,}#", '/', $path);
+    return $path;
+  }
+
+  public function dispatch(string $path, string $method) {
+    $path = $this->normalizePath($path);
+    $method = strtoupper($method);
+    foreach ($this->routes as $route) {
+      if(
+        $route['method'] !== $method ||
+        !preg_match("#^{$route['path']}$#", $path)
+      ) continue;
+      [$class, $function] = $route['controller'];
+      $controllerInterface = new $class;
+      $controllerInterface->$function();
+    }
+  }
+}
